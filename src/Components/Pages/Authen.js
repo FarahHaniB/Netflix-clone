3c7594bea096ed86fc730601a01c74891238bba8
FarahHaniB <109828677+FarahHaniB@users.noreply.github.com>
2023-09-21 14:53:47 +0800

    Removed sensitive info

--- src/Components/Pages/Authen.js
+++ src/Components/Pages/Authen.js
@@ -2,7 +2,7 @@ const authen = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOWE1ZWU5MTk3YjFkMjJjOGIzY2Q5OTk3MDc3YmYxYiIsInN1YiI6IjY1MDZhYjBjM2NkMTJjMDBhZDhiMmNiNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ptQHbd7di5wwNYTosbb10trzJv8C8c2TH5bHR61rWqc'
+      Authorization: 'YOUR_HIDDEN_KEY'
     }
   };
 
